66cef79d06973d50bf6b329b1318e849cd04921d
f.pollet <felixpollet@hotmail.fr>
2025-10-07 16:08:32 +0200

    aviation climate model refactoring

--- aerometrics/climate_models/climate_models.py
+++ aerometrics/climate_models/climate_models.py
@@ -1,44 +1,60 @@
+""" Module containing generic climate model functions for aviation species """
+from typing import Union
+import numpy as np
+import warnings
+
 from aerometrics.climate_models.gwpstar_climate_model import species_gwpstar_climate_model
 from aerometrics.climate_models.lwe_climate_model import species_lwe_climate_model
 from aerometrics.climate_models.fair_climate_model import (
     species_fair_climate_model,
     background_fair_climate_model,
 )
+from aerometrics.climate_models.constants import AVAILABLE_CLIMATE_MODELS, AVAILABLE_SPECIES, SPECIES_SETTINGS
 
 
 def aviation_climate_model(
-    start_year,
-    end_year,
-    species_climate_model,
-    species_quantities,
-    species_settings,
-    model_settings,
+    start_year: int,
+    end_year: int,
+    climate_model: Union[str, callable],
+    species_quantities: dict,
+    species_settings: dict,
+    model_settings: dict,
 ):
+    """
+    Generic climate model calculating RF, ERF and temperature increase for all CO2 and non-CO2 species
+    from aviation emissions.
+    :param start_year: start year of the assessment
+    :param end_year: end year of the assessment
+    :param climate_model: climate model. Must be one of AVAILABLE_CLIMATE_MODELS or a callable function
+    :param species_quantities: dictionary {species: array of annual emissions/forcing values} with species as in AVAILABLE_SPECIES
+    :param species_settings: dictionary {species: {species_setting: value}} with species as in AVAILABLE_SPECIES and species_setting as in SPECIES_SETTINGS
+    :param model_settings: dictionary {model_setting: value}
+    :return:
+    """
 
-    params_species = {}
-    sensitivity_rf = species_settings["sensitivity_rf"]
-    ratio_erf_rf = species_settings["ratio_erf_rf"]
-    efficacy_erf = species_settings["efficacy_erf"]
-    for k in range(0, 7):
-        params_species[k] = {
-            "sensitivity_rf": sensitivity_rf[k],
-            "ratio_erf_rf": ratio_erf_rf[k],
-            "efficacy_erf": efficacy_erf[k],
-        }
+    # --- Check inputs ---
+    check_inputs(
+        start_year,
+        end_year,
+        climate_model,
+        species_quantities,
+        species_settings,
+        model_settings
+    )
 
+    # --- Prepare parameters ---
     params_model = model_settings.copy()
+    if climate_model == "GWP*":
+        climate_model = species_gwpstar_climate_model
 
-    if species_climate_model == "GWP*":
-        species_climate_model = species_gwpstar_climate_model
+    elif climate_model == "LWE":
+        climate_model = species_lwe_climate_model
 
-    elif species_climate_model == "LWE":
-        species_climate_model = species_lwe_climate_model
-
-    elif species_climate_model == "FaIR":
-        species_climate_model = species_fair_climate_model
+    elif climate_model == "FaIR":
+        climate_model = species_fair_climate_model
         background_effective_radiative_forcing, background_temperature = (
             background_fair_climate_model(
-                start_year, end_year, params_species[0], model_settings
+                start_year, end_year, species_settings['Aviation CO2'], model_settings
             )
         )
         params_model["background_effective_radiative_forcing"] = (
@@ -46,182 +62,116 @@ def aviation_climate_model(
         )
         params_model["background_temperature"] = background_temperature
 
-    else:
-        print(
-            "The chosen climate model is directly provided or not available in AeroMAPS."
-        )
-
-    # CO2
-    co2_rf, co2_erf, temperature_increase_from_co2_from_aviation = (
-        species_climate_model(
+    # --- Run model for all species ---
+    results = {}
+    for species in species_quantities.keys():
+        rf, erf, temperature_increase = climate_model(
             start_year,
             end_year,
-            "Aviation CO2",
-            species_quantities[0],
-            params_species[0],
+            species,
+            species_quantities[species],
+            species_settings[species],
             params_model,
         )
-    )
+        results[species] = {
+            "rf": rf,
+            "erf": erf,
+            "temperature increase": temperature_increase
+        }
+    # Species not included in the assessment are set to zero
+    for species in AVAILABLE_SPECIES:
+        if species not in results:
+            warnings.warn(f"{species} not provided, setting contribution to zero")
+            results[species] = {
+                "rf": np.zeros(end_year - start_year + 1),
+                "erf": np.zeros(end_year - start_year + 1),
+                "temperature increase": np.zeros(end_year - start_year + 1)
+            }
 
-    # Contrails
-    contrails_rf, contrails_erf, temperature_increase_from_contrails_from_aviation = (
-        species_climate_model(
-            start_year,
-            end_year,
-            "Aviation contrails",
-            species_quantities[1],
-            params_species[1],
-            params_model,
+    # --- NOX-CH4 effects: discriminate between direct CH4 decrease and O3/H2O variations induced by CH4 decrease ---
+    f1 = 0.5  # Indirect effect of CH4 decrease on ozone
+    f2 = 0.15  # Indirect effect of CH4 decrease on stratospheric water
+    results["Aviation NOX - CH4 decrease"] = {
+        quantity: results["Aviation NOx - CH4 decrease and induced"][quantity] * (1 / (1 + f1 + f2)) for quantity in
+        ["rf", "erf", "temperature increase"]
+    }
+    results["Aviation NOX - CH4 induced O3"] = {
+        quantity: results["Aviation NOx - CH4 decrease and induced"][quantity] * (f1 / (1 + f1 + f2)) for quantity in
+        ["rf", "erf", "temperature increase"]
+    }
+    results["Aviation NOX - CH4 induced H2O"] = {
+        quantity: results["Aviation NOx - CH4 decrease and induced"][quantity] * (f2 / (1 + f1 + f2)) for quantity in
+        ["rf", "erf", "temperature increase"]
+    }
+
+    # --- Aggregate intermediate results ---
+    nox_keys = ["Aviation NOx - ST O3 increase", "Aviation NOx - CH4 decrease and induced"]
+    results["Aviation NOx"] = {
+        quantity: sum(results[key][quantity] for key in nox_keys)
+        for quantity in ["rf", "erf", "temperature increase"]
+    }
+
+    aerosol_keys = ["Aviation soot", "Aviation sulfur"]
+    results["Aviation aerosols"] = {
+        quantity: sum(results[key][quantity] for key in aerosol_keys)
+        for quantity in ["rf", "erf", "temperature increase"]
+    }
+
+    # --- Aggregate results ---
+    non_co2_keys = ["Aviation contrails", "Aviation NOx", "Aviation H2O", "Aviation aerosols"]
+    results["Aviation non-CO2"] = {
+        quantity: sum(results[key][quantity] for key in non_co2_keys)
+        for quantity in ["rf", "erf", "temperature increase"]
+    }
+
+    total_keys = ["Aviation CO2", "Aviation non-CO2"]
+    results["Aviation total"] = {
+        quantity: sum(results[key][quantity] for key in total_keys)
+        for quantity in ["rf", "erf", "temperature increase"]
+    }
+
+    return results
+
+
+def check_inputs(
+        start_year: int,
+        end_year: int,
+        climate_model: Union[str, callable],
+        species_quantities: dict,
+        species_settings: dict,
+        model_settings: dict
+):
+    # Check consistency of start and end year
+    if end_year <= start_year:
+        raise ValueError("end_year must be greater than start_year")
+
+    # Check availability of climate model
+    if climate_model not in AVAILABLE_CLIMATE_MODELS and not callable(climate_model):
+        raise ValueError(
+            f"Climate model must be one of {AVAILABLE_CLIMATE_MODELS} or a callable function"
         )
-    )
 
-    # NOx - ST O3 increase
-    nox_st_o3_rf, nox_st_o3_erf, temperature_increase_from_nox_st_o3_from_aviation = (
-        species_climate_model(
-            start_year,
-            end_year,
-            "Aviation NOx - ST O3 increase",
-            species_quantities[2],
-            params_species[2],
-            params_model,
-        )
-    )
+    # Check species_quantities is in the form {species: array of annual emissions/forcing values}
+    for key, value in species_quantities.items():
+        if key not in AVAILABLE_SPECIES:
+            raise ValueError(f"species_quantities key {key} must be one of {AVAILABLE_SPECIES}")
+        if not isinstance(value, (list, np.ndarray)):
+            raise ValueError(f"species_quantities for {key} must be an array")
+        if len(value) != end_year - start_year + 1:
+            raise ValueError(f"species_quantities for {key} must have length {end_year - start_year + 1}")
 
-    # NOx - CH4 decrease and induced
-    nox_ch4_rf, nox_ch4_erf, temperature_increase_from_nox_ch4_from_aviation = (
-        species_climate_model(
-            start_year,
-            end_year,
-            "Aviation NOx - CH4 decrease and induced",
-            species_quantities[3],
-            params_species[3],
-            params_model,
-        )
-    )
-    nox_rf = nox_st_o3_rf + nox_ch4_rf
-    nox_erf = nox_st_o3_erf + nox_ch4_erf
-    temperature_increase_from_nox_from_aviation = (
-        temperature_increase_from_nox_st_o3_from_aviation
-        + temperature_increase_from_nox_ch4_from_aviation
-    )
-    f1 = 0.5  # Indirect effect on ozone
-    f2 = 0.15  # Indirect effect on stratospheric water
-    nox_ch4_decrease_rf = nox_ch4_rf * (1 / (1 + f1 + f2))
-    nox_ch4_decrease_erf = nox_ch4_erf * (1 / (1 + f1 + f2))
-    temperature_increase_from_nox_ch4_decrease_from_aviation = (
-        temperature_increase_from_nox_ch4_from_aviation * (1 / (1 + f1 + f2))
-    )
-    nox_ch4_o3_rf = nox_ch4_rf * (f1 / (1 + f1 + f2))
-    nox_ch4_o3_erf = nox_ch4_erf * (f1 / (1 + f1 + f2))
-    temperature_increase_from_nox_ch4_o3_from_aviation = (
-        temperature_increase_from_nox_ch4_from_aviation * (f1 / (1 + f1 + f2))
-    )
-    nox_ch4_h2o_rf = nox_ch4_rf * (f2 / (1 + f1 + f2))
-    nox_ch4_h2o_erf = nox_ch4_erf * (f2 / (1 + f1 + f2))
-    temperature_increase_from_nox_ch4_h2o_from_aviation = (
-        temperature_increase_from_nox_ch4_from_aviation * (f2 / (1 + f1 + f2))
-    )
+    # Check species_settings is in the form {species: {species_setting: value}}
+    for key, value in species_settings.items():
+        if key not in AVAILABLE_SPECIES:
+            raise ValueError(f"species_settings key {key} must be one of {AVAILABLE_SPECIES}")
+        for setting in SPECIES_SETTINGS:
+            if setting not in value:
+                raise ValueError(f"species_settings for {key} must contain {setting}")
+            if not isinstance(value[setting], float):
+                raise ValueError(f"species_settings for {key} and {setting} must be a float")
 
-    # H2O
-    h2o_rf, h2o_erf, temperature_increase_from_h2o_from_aviation = (
-        species_climate_model(
-            start_year,
-            end_year,
-            "Aviation H2O",
-            species_quantities[4],
-            params_species[4],
-            params_model,
-        )
-    )
+    # Check consistency of species contained in species_quantities and species_settings
+    if set(species_quantities.keys()) != set(species_settings.keys()):
+        raise ValueError("species_quantities and species_settings must contain the same species")
 
-    # Soot
-    soot_rf, soot_erf, temperature_increase_from_soot_from_aviation = (
-        species_climate_model(
-            start_year,
-            end_year,
-            "Aviation soot",
-            species_quantities[5],
-            params_species[5],
-            params_model,
-        )
-    )
-
-    # Soot
-    sulfur_rf, sulfur_erf, temperature_increase_from_sulfur_from_aviation = (
-        species_climate_model(
-            start_year,
-            end_year,
-            "Aviation sulfur",
-            species_quantities[6],
-            params_species[6],
-            params_model,
-        )
-    )
-    aerosols_rf = soot_rf + sulfur_rf
-    aerosols_erf = soot_erf + sulfur_erf
-    temperature_increase_from_aerosols_from_aviation = (
-        temperature_increase_from_soot_from_aviation
-        + temperature_increase_from_sulfur_from_aviation
-    )
-
-    # Total
-    non_co2_rf = contrails_rf + nox_rf + h2o_rf + soot_rf + sulfur_rf
-    non_co2_erf = contrails_erf + nox_erf + h2o_erf + soot_erf + sulfur_erf
-    temperature_increase_from_non_co2_from_aviation = (
-        temperature_increase_from_contrails_from_aviation
-        + temperature_increase_from_nox_from_aviation
-        + temperature_increase_from_h2o_from_aviation
-        + temperature_increase_from_soot_from_aviation
-        + temperature_increase_from_sulfur_from_aviation
-    )
-    total_rf = co2_rf + non_co2_rf
-    total_erf = co2_erf + non_co2_erf
-    temperature_increase_from_aviation = (
-        temperature_increase_from_co2_from_aviation
-        + temperature_increase_from_non_co2_from_aviation
-    )
-
-    return (
-        co2_rf,
-        co2_erf,
-        temperature_increase_from_co2_from_aviation,
-        contrails_rf,
-        contrails_erf,
-        temperature_increase_from_contrails_from_aviation,
-        nox_rf,
-        nox_erf,
-        temperature_increase_from_nox_from_aviation,
-        nox_st_o3_rf,
-        nox_st_o3_erf,
-        temperature_increase_from_nox_st_o3_from_aviation,
-        nox_ch4_rf,
-        nox_ch4_erf,
-        temperature_increase_from_nox_ch4_from_aviation,
-        nox_ch4_decrease_rf,
-        nox_ch4_decrease_erf,
-        temperature_increase_from_nox_ch4_decrease_from_aviation,
-        nox_ch4_o3_rf,
-        nox_ch4_o3_erf,
-        temperature_increase_from_nox_ch4_o3_from_aviation,
-        nox_ch4_h2o_rf,
-        nox_ch4_h2o_erf,
-        temperature_increase_from_nox_ch4_h2o_from_aviation,
-        h2o_rf,
-        h2o_erf,
-        temperature_increase_from_h2o_from_aviation,
-        aerosols_rf,
-        aerosols_erf,
-        temperature_increase_from_aerosols_from_aviation,
-        soot_rf,
-        soot_erf,
-        temperature_increase_from_soot_from_aviation,
-        sulfur_rf,
-        sulfur_erf,
-        temperature_increase_from_sulfur_from_aviation,
-        non_co2_rf,
-        non_co2_erf,
-        temperature_increase_from_non_co2_from_aviation,
-        total_rf,
-        total_erf,
-        temperature_increase_from_aviation,
-    )
+    return
---
+++ aerometrics/climate_models/constants.py
@@ -0,0 +1,14 @@
+"""
+Constants for the Aerometrics package.
+"""
+
+AVAILABLE_CLIMATE_MODELS = ['GWP*', 'LWE', 'FaIR']
+AVAILABLE_SPECIES = ['Aviation CO2',
+                     'Aviation contrails',
+                     'Aviation NOx - ST O3 increase',
+                     'Aviation NOx - CH4 decrease and induced',
+                     'Aviation soot',
+                     'Aviation sulfur',
+                     'Aviation H2O'
+                     ]
+SPECIES_SETTINGS = ['sensitivity_rf', 'ratio_erf_rf', 'efficacy_erf']
